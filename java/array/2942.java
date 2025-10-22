99c1ceb35606d834c4d9dc807db332327b1e73d9
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-10-21 23:25:15 -0300

    feat (2942): Optimize character search in findWordsContaining method

--- java/array/2942.java
+++ java/array/2942.java
@@ -1,9 +1,10 @@
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         List<Integer> result = new ArrayList<>();
+        String target = String.valueOf(x);
 
         for (int i = 0; i < words.length; i++) {
-            if (words[i].contains(String.valueOf(x))) {
+            if (words[i].contains(target)) {
                 result.add(i);
             }
         }
