c07b4857b0a58143e8227edf002bb663aff83f28
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-10-23 16:25:57 -0300

    feat (kotlin): Implement getConcatenation function in Solution class

---
+++ kotlin/array/1929.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun getConcatenation(nums: IntArray): IntArray {
+        val n = nums.size;
+        val ans = IntArray(n * 2);
+
+        System.arraycopy(nums, 0, ans, 0, n);
+        System.arraycopy(nums, 0, ans, n, n);
+
+        return ans;
+    }
+}
